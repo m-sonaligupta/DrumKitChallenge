0cc5e586564827630e09178bb267f22b77376079
Sonali Gupta <50748546+m-sonaligupta@users.noreply.github.com>
2021-02-09 18:57:19 +0530

    Add files via upload

---
+++ Drum Kit/index.js
@@ -0,0 +1,71 @@
+
+//Detecting Button Press
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+  for(var i = 0; i < numberOfDrumButtons; i++)
+  {
+    document.querySelectorAll(".drum")[i].addEventListener("click",function()
+      {
+      var buttonInnerHTML = this.innerHTML;
+      makeSound(buttonInnerHTML);
+      buttunAnimation(buttonInnerHTML);
+      }
+    );
+  }
+
+  //Detecting Keyboard Press
+  document.addEventListener("keypress", function(event) //made a mistakeof putting keyperss cap
+    {
+      makeSound(event.key);
+      buttunAnimation(event.key);
+    }
+  );
+
+  function makeSound(key)
+  {
+                switch (key)
+                {
+                    case "w":
+                    var tom4 = new Audio("sounds/tom-4.mp3");
+                    tom4.play();
+                    break;
+                    case "a":
+                    var crash = new Audio("sounds/crash.mp3");
+                    crash.play();
+                    break;
+                    case "s":
+                    var tom1 = new Audio("sounds/tom-1.mp3");
+                    tom1.play();
+                    break;
+                    case "d":
+                    var kickBass = new Audio("sounds/kick-bass.mp3");
+                    kickBass.play();
+                    break;
+                    case "j":
+                    var tom2 = new Audio("sounds/tom-2.mp3");
+                    tom2.play();
+                    break;
+                    case "k":
+                    var snare = new Audio("sounds/snare.mp3");
+                    snare.play();
+                    break;
+                    case "l":
+                    var tom3 = new Audio("sounds/tom-3.mp3");
+                    tom3.play();
+                    break;
+
+                    default:
+                    console.log(buttonInnerHTML);
+
+                }
+  }
+
+
+
+      function buttunAnimation(currentKey){
+        var activeButton = document.querySelector("." +currentKey);
+        activeButton.classList.add("pressed");
+        setTimeout(function(){
+          activeButton.classList.remove("pressed");
+        },100);
+      }
